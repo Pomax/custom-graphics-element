48fa3086d84e324b16169aa795c0d821a97eaf0c
Pomax <pomax@nihongoresources.com>
2020-08-02 12:30:20 -0700

    shape drawing

--- custom-element/api/graphics-api.js
+++ custom-element/api/graphics-api.js
@@ -1,14 +1,15 @@
-import { BaseAPI } from "./base-api.js";
 import { enrich } from "../lib/enrich.js";
-import { Point } from "../lib/types/point.js";
-import { Bezier } from "../lib/types/bezier.js";
+import { Point } from "./types/point.js";
+import { Bezier } from "./types/bezier.js";
+import { Shape } from "./util/shape.js";
+import { BaseAPI } from "./base-api.js";
 
 /**
  * Our Graphics API, which is the "public" side of the API.
  */
 class GraphicsAPI extends BaseAPI {
   static get constants() {
-    return [`POINTER`, `HAND`, `PI`, `TAU`];
+    return [`POINTER`, `HAND`, `PI`, `TAU`, `POLYGON`, `CURVE`, `BEZIER`];
   }
 
   get PI() {
@@ -22,6 +23,15 @@ class GraphicsAPI extends BaseAPI {
   }
   get HAND() {
     return `pointer`;
+  }
+  get POLYGON() {
+    return Shape.POLYGON;
+  }
+  get CURVE() {
+    return Shape.CURVE;
+  }
+  get BEZIER() {
+    return Shape.BEZIER;
   }
 
   /**
@@ -60,14 +70,30 @@ class GraphicsAPI extends BaseAPI {
    * Set the context fillStyle
    */
   setFill(color) {
+    if (color === false) color = `transparent`;
     this.ctx.fillStyle = color;
+  }
+
+  /**
+   * Convenience alias
+   */
+  noFill() {
+    this.setFill(false);
   }
 
   /**
    * Set the context strokeStyle
    */
   setStroke(color) {
+    if (color === false) color = `transparent`;
     this.ctx.strokeStyle = color;
+  }
+
+  /**
+   * Convenience alias
+   */
+  noStroke() {
+    this.setStroke(false);
   }
 
   /**
@@ -117,6 +143,110 @@ class GraphicsAPI extends BaseAPI {
    */
   text(str, x, y) {
     this.ctx.fillText(str, x, y);
+  }
+
+  /**
+   * Draw a rectangle start with {p} in the upper left
+   */
+  rect(p, w, h) {
+    this.ctx.fillRect(p.x, p.y, w, h);
+    this.ctx.strokeRect(p.x, p.y, w, h);
+  }
+
+  /**
+   * A signal for starting a complex shape
+   */
+  start(type) {
+    this.currentShape = new Shape(type || this.POLYGON);
+  }
+
+  /**
+   * A signal for starting a new segment of a complex shape
+   */
+  segment(type, factor) {
+    this.currentShape.addSegment(type, factor);
+  }
+
+  /**
+   * Add a plain point to the current shape
+   */
+  vertex(p) {
+    this.currentShape.vertex(p);
+  }
+
+  /**
+   * A signal to draw the current complex shape
+   */
+  end() {
+    this.ctx.beginPath();
+    let first = this.currentShape.first;
+    this.ctx.moveTo(first.x, first.y);
+    this.currentShape.segments.forEach((s) =>
+      this[`draw${s.type}`](this.ctx, s.points, s.factor)
+    );
+    this.ctx.fill();
+    this.ctx.stroke();
+  }
+
+  /**
+   * Yield a snapshot of the current shape.
+   */
+  save() {
+    return this.currentShape;
+  }
+
+  /**
+   * Draw a previously created shape
+   */
+  drawShape(shape) {
+    this.currentShape = shape;
+    this.end();
+  }
+
+  /**
+   * Polygon draw function
+   */
+  drawPolygon(ctx, points) {
+    points.forEach((p) => ctx.lineTo(p.x, p.y));
+  }
+
+  /**
+   * Curve draw function, which draws a CR curve as a series of Beziers
+   */
+  drawCatmullRom(ctx, points, f) {
+    // invent a virtual first and last point
+    const f0 = points[0],
+      f1 = points[1],
+      fn = f0.reflect(f1),
+      l1 = points[points.length - 2],
+      l0 = points[points.length - 1],
+      ln = l0.reflect(l1),
+      cpoints = [fn, ...points, ln];
+
+    // four point sliding window over the segment
+    for (let i = 0, e = cpoints.length - 3; i < e; i++) {
+      let [c1, c2, c3, c4] = cpoints.slice(i, i + 4);
+      let p2 = {
+        x: c2.x + (c3.x - c1.x)/(6*f),
+        y: c2.y + (c3.y - c1.y)/(6*f)
+      };
+      let p3 = {
+        x: c3.x - (c4.x - c2.x)/(6*f),
+        y: c3.y - (c4.y - c2.y)/(6*f)
+      };
+      ctx.bezierCurveTo(p2.x, p2.y, p3.x, p3.y, c3.x, c3.y);
+    }
+  }
+
+  /**
+   * Curve draw function, which assumes Bezier coordinates
+   */
+  drawBezier(ctx, points) {
+    console.log(points);
+    for (let i = 0, e = points.length; i < e; i+=3) {
+      let [p1, p2, p3] = points.slice(i, i + 3);
+      ctx.bezierCurveTo(p1.x, p1.y, p2.x, p2.y, p3.x, p3.y);
+    }
   }
 }
 

21d82379684b5b81c201045c4ae118293603c248
Pomax <pomax@nihongoresources.com>
2023-12-30 10:40:37 -0800

    pre-v1.2

--- finish-up.js
+++ finish-up.js
@@ -1,8 +1,9 @@
 import fs from "node:fs";
-const { version } = JSON.parse(fs.readFileSync(`./package.json`));
+
 const api = fs
   .readFileSync(`./graphics-element/graphics-api.js`)
   .toString(`utf-8`);
+
 const lib = fs.readFileSync(`./dist/graphics-element.js`).toString(`utf-8`);
 
 function base64(data) {
@@ -13,12 +14,3 @@ function base64(data) {
 
 const updated = lib.replace(`THIS_IS_A_PLACEHOLDER`, base64(api));
 fs.writeFileSync(`./dist/graphics-element.js`, updated, `utf-8`);
-
-fs.renameSync(
-  `./dist/graphics-element.js`,
-  `./dist/graphics-element-${version}.js`
-);
-fs.renameSync(
-  `./dist/graphics-element.css`,
-  `./dist/graphics-element-${version}.css`
-);

dc66e2a54cf7e83ad589860586564ba70ea2c5f2
Pomax <pomax@nihongoresources.com>
2023-12-31 12:41:05 -0800

    update the demo

--- edit/editor.js
+++ edit/editor.js
@@ -24,75 +24,89 @@ const demoCode = `let airplane;
  * change heading when told to.
  */
 class Airplane extends Circle {
-	speed = 0.5;
-	heading = -0.9;
-	turnRate = (this.speed / 100) * 3.1415;
+  speed = 0.5;
+  heading = -0.9;
+  bank = 0;
+  turnRate = (this.speed / 100) * 3.1415;
 
-	update({ x, y }) {
-		const angle = atan2(y - this.y, x - this.x);
-		this.setHeading(angle);
-		const { speed, heading } = this;
-		this.x += speed * cos(heading);
-		this.y += speed * sin(heading);
-	}
+  // update the plane's position
+  update({ x, y }) {
+    const angle = atan2(y - this.y, x - this.x);
+    this.setHeading(angle);
+    const { speed, heading } = this;
+    this.x += speed * cos(heading);
+    this.y += speed * sin(heading);
+  }
 
-	setHeading(target) {
-		const { heading, turnRate } = this;
-		let diff = (target - heading + TAU) % TAU;
-		if (diff > PI) diff -= TAU;
-		this.heading += constrain(diff, -turnRate, turnRate);
-	}
-
-	intersection(x1, y1, x2, y2) {
-		const { x, y, r } = this;
-		const dy = y2 - y1;
-		const dx = x2 - x1;
-		const c = new Point(this);
-
-		const A = dy * dy + dx * dx;
-		const B = 2 * (-c.x * dx - c.y * dy + x1 * dx + y1 * dy);
-		const C =
-			c.x * c.x +
-			c.y * c.y +
-			x1 * x1 +
-			y1 * y1 -
-			2 * c.x * x1 -
-			2 * c.y * y1 -
-			r * r;
-		const D = B * B - 4 * A * C;
-
-		const t1 = (-B + sqrt(D)) / (2 * A);
-		const t2 = (-B - sqrt(D)) / (2 * A);
-
-		if (isNaN(t1) && isNaN(t2)) {
-			const cx = x - x1;
-			const cy = y - y1;
-			const f = constrain((dx * cx + dy * cy) / (dx * dx + dy * dy), 0, 1);
-			return new Point(x1 + dx * f, y1 + dy * f);
-		}
-
-		if (isNaN(t1) || t1 < t2) t1 = t2;
-		const t = constrain(t1, 0, 1);
-		return new Point(x1 + dx * t, y1 + dy * t);
-	}
+  // update the plane's heading based on a target point
+  setHeading(target) {
+    const { heading, turnRate } = this;
+    let diff = (target - heading + TAU) % TAU;
+    if (diff > PI) diff -= TAU;
+    this.heading += constrain(diff, -turnRate, turnRate);
+  }
 
   // draw a little "stick figure" airplane
-	draw() {
-		const { x, y, r, heading: a } = this;
-		save();
-		translate(x, y);
-		rotate(a);
-		setColor(\`black\`);
-		setLineWidth(3);
-		line(-10,0,10,0);
-		line(-10,-5,-10,5);
-		line(2,0,0,-12);
-		line(2,0,0,12);
-		setColor(\`blue\`);
-		setLineWidth(1);
-		line(0,0,this.speed * 100,0);
-		restore();
-	}
+  draw() {
+    const { x, y, r, heading: a, bank, turnRate} = this;
+    save();
+    {
+      translate(x, y);
+      rotate(a);
+      setColor(\`black\`);
+      setLineWidth(3);
+      line(-10, 0, 10, 0);
+      line(-10,-5,-10, 5);
+      const wingLength = map(abs(bank), 0, turnRate, 12, 10);
+      save();
+      {
+        rotate(PI/2);
+        line(0, 0, wingLength, 0);
+        rotate(PI);
+        line(0, 0, wingLength, 0);
+      }
+      restore();
+      setColor(\`blue\`);
+      setLineWidth(1);
+      line(0,0,this.speed * 100,0);
+    }
+    restore();
+  }
+
+  // Determine where on the line p1--p2 this airplane should
+  // be projected, based on the shorted projection distance.
+  project(x1, y1, x2, y2) {
+    const { x, y, r } = this;
+    const dy = y2 - y1;
+    const dx = x2 - x1;
+    const c = new Point(this);
+
+    const A = dy * dy + dx * dx;
+    const B = 2 * (-c.x * dx - c.y * dy + x1 * dx + y1 * dy);
+    const C =
+      c.x * c.x +
+      c.y * c.y +
+      x1 * x1 +
+      y1 * y1 -
+      2 * c.x * x1 -
+      2 * c.y * y1 -
+      r * r;
+    const D = B * B - 4 * A * C;
+
+    const t1 = (-B + sqrt(D)) / (2 * A);
+    const t2 = (-B - sqrt(D)) / (2 * A);
+
+    if (isNaN(t1) && isNaN(t2)) {
+      const cx = x - x1;
+      const cy = y - y1;
+      const f = constrain((dx * cx + dy * cy) / (dx * dx + dy * dy), 0, 1);
+      return new Point(x1 + dx * f, y1 + dy * f);
+    }
+
+    if (isNaN(t1) || t1 < t2) t1 = t2;
+    const t = constrain(t1, 0, 1);
+    return new Point(x1 + dx * t, y1 + dy * t);
+  }
 }
 
 let current = -1;
@@ -103,53 +117,53 @@ const trail = [];
  * Our program entry point.
  */
 function setup() {
-	setSize(650, 500);
-	addButton(\`play\`, (button) => {
-		button.textContent = togglePlay() ? \`pause\` : \`play\`;
-	});
-	addButton(\`reset\`, (button) => {
-		points.splice(0, points.length);
-		trail.splice(0, trail.length);
-		reset();
-	});
-	noGrid();
-	airplane = new Airplane(100, 100, 40);
-	addPoint(476, 100);
-	addPoint(482, 425);
-	addPoint(319, 264);
-	addPoint(146, 393);
+  setSize(650, 500);
+  addButton(\`play\`, (button) => {
+    button.textContent = togglePlay() ? \`pause\` : \`play\`;
+  });
+  addButton(\`reset\`, (button) => {
+    points.splice(0, points.length);
+    trail.splice(0, trail.length);
+    reset();
+  });
+  noGrid();
+  airplane = new Airplane(100, 100, 40);
+  addPoint(476, 100);
+  addPoint(482, 425);
+  addPoint(319, 264);
+  addPoint(146, 393);
 }
 
 /**
  * The draw loop entry point.
  */
 function draw() {
-	clear(\`#FFEFB0\`);
+  clear(\`#FFEFB0\`);
 
-	noFill();
-	setStroke(\`lightgrey\`);
-	plotData(points, \`x\`, \`y\`);
+  noFill();
+  setStroke(\`lightgrey\`);
+  plotData(points, \`x\`, \`y\`);
 
-	setStroke(\`blue\`);
-	plotData(trail, \`x\`, \`y\`);
+  setStroke(\`blue\`);
+  plotData(trail, \`x\`, \`y\`);
 
-	setStroke(\`black\`);
-	points.forEach((p) => point(p.x, p.y));
+  setStroke(\`black\`);
+  points.forEach((p) => point(p.x, p.y));
 
-	// figure out where to fly to
-	const target = getTarget();
-	if (target) {
-		setStroke(\`green\`);
-		line(airplane.x, airplane.y, target.x, target.y);
-		setStroke(\`black\`);
-		if (playing) airplane.update(target);
-	}
+  // figure out where to fly to
+  const target = getTarget();
+  if (target) {
+    setStroke(\`green\`);
+    line(airplane.x, airplane.y, target.x, target.y);
+    setStroke(\`black\`);
+    if (playing) airplane.update(target);
+  }
 
-	trail.push(new Point(airplane.x, airplane.y));
+  trail.push(new Point(airplane.x, airplane.y));
 
-	noFill();
-	setStroke(\`black\`);
-	airplane.draw();
+  noFill();
+  setStroke(\`black\`);
+  airplane.draw();
 }
 
 /**
@@ -157,24 +171,24 @@ function draw() {
  * At its most basic, though (for demo purposes) we just target "the next point".
  */
 function getTarget() {
-	if (current < 0) return;
+  if (current < 0) return;
 
-	let target = points[current];
-	if (!target) return;
+  let target = points[current];
+  if (!target) return;
 
-	if (dist(airplane.x, airplane.y, target.x, target.y) < airplane.r / 10) {
-		current++;
-	}
+  if (dist(airplane.x, airplane.y, target.x, target.y) < airplane.r / 10) {
+    current++;
+  }
 
-	return target;
+  return target;
 }
 
 /**
  * If we click on the graphic, place a new point.
  */
 function pointerDown(x, y) {
-	addPoint(x, y);
-	redraw();
+  addPoint(x, y);
+  redraw();
 }
 
 /**
@@ -183,13 +197,13 @@ function pointerDown(x, y) {
  * the first point as our current target.
  */
 function addPoint(x, y) {
-	if (currentPoint) return;
-	const p = new Point(x, y);
-	setMovable(p);
-	points.push(p);
-	if (current === -1 && points.length === 1) {
-		current = 0;
-	}
+  if (currentPoint) return;
+  const p = new Point(x, y);
+  setMovable(p);
+  points.push(p);
+  if (current === -1 && points.length === 1) {
+    current = 0;
+  }
 }
 `;
 const savedCode = localStorage.getItem(`code`) || demoCode;
@@ -206,7 +220,11 @@ const editor = monaco.editor.create(document.getElementById(`editor`), {
 });
 
 // let's keep code nice and concise.
-editor.getModel().updateOptions({ tabSize: 2 });
+editor.getModel().updateOptions({
+  detectIndentation: true,
+  insertSpaces: true,
+  tabSize: 2,
+});
 
 // Add update debouncing, so we don't refresh on every keystroke.
 let runUpdate = -1;

e52892632d3b8d80575668180543cc4a2fdf9c14
Pomax <pomax@nihongoresources.com>
2024-03-08 14:45:58 -0800

    docs for disablers

--- graphics-element/api/parts/disablers.js
+++ graphics-element/api/parts/disablers.js
@@ -1,5 +1,31 @@
 /**
- * Ensure that there is no border around the canvas element
+ * Ensure that there is no border around the canvas element.
+ *
+ * Example:
+ *
+ * <graphics-element>
+ *   <graphics-source>
+ *     function setup() {
+ *       setSize(200, 200);
+ *       setBorder(5, `red`);
+ *     }
+ *     function draw() {
+ *       clear(`white`);
+ *       setColor(`black`);
+ *       setTextAlign(CENTER, CENTER);
+ *       setFontSize(25);
+ *       text(`click me`, width/2, height/2);
+ *     }
+ *     function pointerUp(x, y) {
+ *       setBorder(5, `red`);
+ *       redraw()
+ *     }
+ *     function pointerDown(x, y) {
+ *       noBorder();
+ *       redraw()
+ *     }
+ *   </graphics-source>
+ * </graphics-element>
  */
 function noBorder() {
   setBorder(false);
@@ -7,6 +33,25 @@ function noBorder() {
 
 /**
  * Disable both stroke and fill color.
+ *
+ * Example:
+ *
+ * <graphics-element>
+ *   <graphics-source>
+ *     function setup() {
+ *       setSize(200, 200);
+ *     }
+ *     function draw() {
+ *       clear(`white`);
+ *       setColor(`black`);
+ *       setTextAlign(CENTER, CENTER);
+ *       setFontSize(25);
+ *       text(`Now you see me`, width/2, height/2 - 25);
+ *       noColor();
+ *       text(`Now you don't`, width/2, height/2 + 25);
+ *     }
+ *   </graphics-source>
+ * </graphics-element>
  */
 function noColor() {
   noFill();
@@ -15,6 +60,30 @@ function noColor() {
 
 /**
  * Hide the cursor.
+ *
+ * Example:
+ *
+ * <graphics-element>
+ *   <graphics-source>
+ *     function setup() {
+ *       setSize(200, 200);
+ *     }
+ *     function draw() {
+ *       clear(`white`);
+ *       setColor(`#FF02`);
+ *       rect(0, 0, width/2, height);
+ *       setColor(`#0FF2`);
+ *       rect(width/2, 0, width/2, height);
+ *     }
+ *     function pointerMove(x, y) {
+ *       if (x < width/2) {
+ *         setCursor(AUTO);
+ *       } else {
+ *         noCursor();
+ *       }
+ *     }
+ *   </graphics-source>
+ * </graphics-element>
  */
 function noCursor() {
   __canvas.style.cursor = `none`;
@@ -22,6 +91,28 @@ function noCursor() {
 
 /**
  * Disable the fill color.
+ *
+ * Example:
+ *
+ * <graphics-element>
+ *   <graphics-source>
+ *     function setup() {
+ *       setSize(200, 200);
+ *     }
+ *     function draw() {
+ *       clear(`white`);
+ *       setTextAlign(CENTER, CENTER);
+ *       setFontSize(25);
+ *       setTextStroke(1);
+ *       setColor(`black`);
+ *       rect(20, 70, 20, 20);
+ *       text(`filled`, width/2, height/2 - 25);
+ *       noFill();
+ *       rect(30, 80, 20, 20);
+ *       text(`not filled`, width/2, height/2 + 25);
+ *     }
+ *   </graphics-source>
+ * </graphics-element>
  */
 function noFill() {
   setFill(`transparent`);
@@ -29,6 +120,31 @@ function noFill() {
 
 /**
  * Disable the default grid background.
+ *
+ * Example:
+ *
+ * <graphics-element>
+ *   <graphics-source>
+ *     function setup() {
+ *       setSize(200, 200);
+ *     }
+ *     function draw() {
+ *       clear(`white`);
+ *       setColor(`black`);
+ *       setTextAlign(CENTER, CENTER);
+ *       setFontSize(25);
+ *       text(`click me`, width/2, height/2);
+ *     }
+ *     function pointerUp(x, y) {
+ *       setGrid(20, `lightgrey`);
+ *       redraw()
+ *     }
+ *     function pointerDown(x, y) {
+ *       noGrid();
+ *       redraw()
+ *     }
+ *   </graphics-source>
+ * </graphics-element>
  */
 function noGrid() {
   __draw_grid = false;
@@ -36,27 +152,88 @@ function noGrid() {
 
 /**
  * Set the line stroke to "solid".
+ *
+ * Example:
+ *
+ * <graphics-element>
+ *   <graphics-source>
+ *     function setup() {
+ *       setSize(200, 200);
+ *     }
+ *     function draw() {
+ *       clear(`white`);
+ *       setColor(`black`);
+ *       setLineDash(1);
+ *       line(0,20,width,50);
+ *       setLineDash(5);
+ *       line(0,30,width,90);
+ *       setLineDash(1,2,3,4);
+ *       line(0,40,width,130);
+ *       noLineDash();
+ *       line(0,50,width,180);
+ *     }
+ *   </graphics-source>
+ * </graphics-element>
  */
 function noLineDash() {
   __ctx.setLineDash([]);
 }
 
 /**
- * Disable the box shadow.
- */
-function noShadow() {
-  setShadow(`transparent`, 0);
-}
-
-/**
  * Disable the stroke color.
+ *
+ * Example:
+ *
+ * <graphics-element>
+ *   <graphics-source>
+ *     function setup() {
+ *       setSize(200, 200);
+ *     }
+ *     function draw() {
+ *       clear(`white`);
+ *       setTextAlign(CENTER, CENTER);
+ *       setFontSize(25);
+ *       setTextStroke(1);
+ *       setColor(`red`);
+ *       setStroke(`black`);
+ *       rect(20, 70, 20, 20);
+ *       text(`stroked`, width/2, height/2 - 25);
+ *       noStroke();
+ *       rect(42, 82, 20, 20);
+ *       text(`not stroked`, width/2, height/2 + 25);
+ *     }
+ *   </graphics-source>
+ * </graphics-element>
  */
 function noStroke() {
   setStroke(`transparent`);
 }
 
 /**
- * Disable text stroking.
+ * Disable text stroking, but not regular shape stroking.
+ *
+ * Example:
+ *
+ * <graphics-element>
+ *   <graphics-source>
+ *     function setup() {
+ *       setSize(200, 200);
+ *     }
+ *     function draw() {
+ *       clear(`white`);
+ *       setTextAlign(CENTER, CENTER);
+ *       setFontSize(25);
+ *       setTextStroke(1);
+ *       setColor(`red`);
+ *       setStroke(`black`);
+ *       rect(20, 70, 20, 20);
+ *       text(`stroked`, width/2, height/2 - 25);
+ *       noStroke();
+ *       rect(42, 82, 20, 20);
+ *       text(`not stroked`, width/2, height/2 + 25);
+ *     }
+ *   </graphics-source>
+ * </graphics-element>
  */
 function noTextStroke() {
   setTextStroke(false, undefined);
--- graphics-element/base.d.ts
+++ graphics-element/base.d.ts
@@ -6,3 +6,115 @@ declare function setup(): void;
  *
  */
 declare function draw(): void;
+/**
+ *
+ * @param trueOrFalse
+ */
+declare function pointerActive(trueOrFalse: boolean): void;
+/**
+ *
+ * @param x
+ * @param y
+ */
+declare function pointerDown(x: number, y: number): void;
+/**
+ *
+ * @param x
+ * @param y
+ */
+declare function pointerUp(x: number, y: number): void;
+/**
+ *
+ * @param x
+ * @param y
+ */
+declare function pointerClick(x: number, y: number): void;
+/**
+ *
+ * @param x
+ * @param y
+ */
+declare function pointerMove(x: number, y: number): void;
+/**
+ *
+ * @param x
+ * @param y
+ */
+declare function pointerDrag(x: number, y: number): void;
+/**
+ *
+ * @param key
+ * @param shift
+ * @param alt
+ * @param ctrl
+ * @param meta
+ */
+declare function keyDown(
+  key: string,
+  shift: boolean,
+  alt: boolean,
+  ctrl: boolean,
+  meta: boolean
+): void;
+/**
+ *
+ * @param key
+ * @param shift
+ * @param alt
+ * @param ctrl
+ * @param meta
+ */
+declare function keyUp(
+  key: string,
+  shift: boolean,
+  alt: boolean,
+  ctrl: boolean,
+  meta: boolean
+): void;
+/**
+ *
+ */
+declare type width = number;
+/**
+ *
+ */
+declare type height = number;
+/**
+ *
+ */
+declare type playing = boolean;
+/**
+ *
+ */
+declare type frame = number;
+/**
+ *
+ */
+declare type frameDelta = number;
+/**
+ *
+ */
+declare type currentPoint = { x: number; y: number };
+/**
+ *
+ */
+declare type pointer = {
+  active: boolean;
+  down: boolean;
+  drag: boolean;
+  mark:
+    | undefined
+    | {
+        x: number;
+        y: number;
+      };
+  x: number;
+  y: number;
+};
+
+/**
+ *
+ */
+declare type keyboard = {
+  [letter: string]: number;
+};
